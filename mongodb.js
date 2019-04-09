4236b9da5e9ac7ded76ce7db16cc2939c36f95da
Vasserman2000 <vasserman2000@gmail.com>
2019-04-09 23:09:53 +0300

    mongo find and findOne

--- mongodb.js
+++ mongodb.js
@@ -14,7 +14,37 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    
+
+    // select one
+   db.collection('users').findOne({
+       name: 'Lea'
+   }, (error, user) => {
+       if (error) {
+           return console.log('Unable to fetch');
+       }
+
+       console.log(user);
+   });
+
+   // select multiple
+   db.collection('users').find({age: 33}).toArray((error, users) => {
+       console.log(users);
+   });
+
+   // count fetched records
+   db.collection('users').find({age: 33}).count((error, users) => {
+       console.log(users);
+    });
+
+    // fetch by id
+    db.collection('tasks').findOne({_id: new ObjectID('5cacaadbe01a29371ccc5145')}, (error, task) => {
+        console.log(task);
+    });
+
+    // fetch multiple by parameter
+    db.collection('tasks').find({completed: false}).toArray((error, task) => {
+        console.log(task);
+    });
 
     client.close();
 });

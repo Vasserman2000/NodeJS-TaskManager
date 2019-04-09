994ba104e5d0df7c59028e7d7aaf24be32fe7248
Vasserman2000 <vasserman2000@gmail.com>
2019-04-09 22:15:24 +0300

    mongoDb

---
+++ helpers/mongodb-insert.js
@@ -0,0 +1,45 @@
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+
+    // insert one
+    db.collection('users').insertOne({
+        name: 'Moshe',
+        age: 33
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user!');
+        }
+        console.log(result.ops);
+    });
+
+
+    // insert many
+    db.collection('users').insertMany([
+        {
+            name: 'Elisha',
+            age: 18
+        },
+        {
+            name: 'Lea',
+            age: 15,
+            status: 'Married'
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert to db.')
+        }
+        console.log(result.insertedIds);
+    });
+      
+    client.close();
+});
--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,11 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+
+
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -11,8 +14,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 33
-    })
-})
+    
+
+    client.close();
+});

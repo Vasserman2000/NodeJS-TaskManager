2593e93cbeee2546f39c1c343ab270cf08319cfb
Vasserman2000 <vasserman2000@gmail.com>
2019-04-22 23:08:28 +0300

    promise chaining

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,5 +4,6 @@ const task = require('../models/task')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 });

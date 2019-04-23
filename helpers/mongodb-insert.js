ba577f6ca0ffd84d649c5c092605a7be129b7138
Vasserman2000 <vasserman2000@gmail.com>
2019-04-23 22:35:50 +0300

    continuing playing with mongooseJs querying

--- helpers/mongodb-insert.js
+++ helpers/mongodb-insert.js
@@ -1,7 +1,7 @@
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'task-manager';
+const databaseName = 'task-manager-api';
 
-
+const MongoClient = require('mongodb')
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -12,34 +12,45 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
 
     // insert one
-    db.collection('users').insertOne({
-        name: 'Moshe',
-        age: 33
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user!');
-        }
-        console.log(result.ops);
-    });
+    // db.collection('users').insertOne({
+    //     name: 'Moshe',
+    //     age: 33
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user!');
+    //     }
+    //     console.log(result.ops);
+    // });
 
 
     // insert many
-    db.collection('users').insertMany([
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Elisha',
+    //         age: 18
+    //     },
+    //     {
+    //         name: 'Lea',
+    //         age: 15,
+    //         status: 'Married'
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert to db.')
+    //     }
+    //     console.log(result.insertedIds);
+    // });
+
+    db.collection('tasks').insertMany([
         {
-            name: 'Elisha',
-            age: 18
-        },
-        {
-            name: 'Lea',
-            age: 15,
-            status: 'Married'
+            description: 'make a coffee',
+            completed: true
         }
     ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert to db.')
-        }
-        console.log(result.insertedIds);
-    });
+            if (error) {
+                return console.log('Unable to insert to db.')
+            }
+            console.log(result.insertedIds);});
       
     client.close();
 });
--- helpers/promise-chaining.js
+++ helpers/promise-chaining.js
@@ -1,11 +1,33 @@
-require('../src/db/mongoose');
+const mongoose = require('../src/db/mongoose');
 const User = require('../src/models/user').User;
+const Task = require('../src/models/task').Task;
+const db = mongoose.db;
 
-User.findByIdAndUpdate('5caf99995aefe12c784ab3e3', { age: 1 }).then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 19 });
-}).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
-});
+// User.findByIdAndUpdate('5caf99995aefe12c784ab3e3', { age: 1 }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 19 });
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+/*******************************/
+
+Task.findByIdAndDelete('5cbf684f5c391112f820b8cd')
+    .then((task) => {
+        if (!task) {
+            return console.log('no such task found')
+        }
+        console.log(task);
+
+        Task.find({ completed: false }, (err, tasks) => {
+            if (tasks.length == 0) {
+                return console.log('no tasks that match your criteria')
+            }
+            console.log(tasks);
+        })
+    })
+    .catch((e)=>{console.log(e.message)});
+
+    

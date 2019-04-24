dfb3ec907fd212afce3ae054f9c3744d69622e1e
Vasserman2000 <vasserman2000@gmail.com>
2019-04-24 16:00:13 +0300

    promises chaining continuing

--- helpers/promise-chaining.js
+++ helpers/promise-chaining.js
@@ -1,33 +1,37 @@
-const mongoose = require('../src/db/mongoose');
+mongoose = require('../src/db/mongoose');
 const User = require('../src/models/user').User;
 const Task = require('../src/models/task').Task;
-const db = mongoose.db;
 
-// User.findByIdAndUpdate('5caf99995aefe12c784ab3e3', { age: 1 }).then((user) => {
-//     console.log(user);
-//     return User.countDocuments({ age: 19 });
-// }).then((result) => {
-//     console.log(result);
-// }).catch((e) => {
-//     console.log(e);
-// });
 
-/*******************************/
+User.findByIdAndUpdate('5caf99995aefe12c784ab3e3', { age: 1 }).then((user) => {
+    console.log(user);
+    return User.countDocuments({ age: 19 });
+}).then((result) => {
+    console.log(result);
+}).catch((e) => {
+    console.log(e);
+});
 
-Task.findByIdAndDelete('5cbf684f5c391112f820b8cd')
-    .then((task) => {
-        if (!task) {
-            return console.log('no such task found')
+
+Task.findByIdAndDelete('5cc0552843fceda17d4d0867').then((task) => {
+    if (!task) {
+        return console.log('no such task found')
+    }
+    console.log(task);
+
+    return Task.find({ completed: true }, (err, tasks) => {
+        if (tasks.length == 0) {
+            console.log('no tasks that match your criteria')
         }
-        console.log(task);
+        console.log(tasks);
+    });
 
-        Task.find({ completed: false }, (err, tasks) => {
-            if (tasks.length == 0) {
-                return console.log('no tasks that match your criteria')
-            }
-            console.log(tasks);
-        })
-    })
-    .catch((e)=>{console.log(e.message)});
+    // return Task.findByIdAndUpdate('5cbf684f5c391112f820b8c', { completed: true }).then((task) => {
+    //     if (!task) {
+    //         console.log('could not find such task');
+    //     }
+    //     console.log(task);
+    // });
+}).catch((e)=>{console.log(e.message)});
 
     

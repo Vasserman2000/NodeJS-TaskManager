bb49d0d60f8346eec13b2960fb84fcccca7ad064
Vasserman2000 <vasserman2000@gmail.com>
2019-04-25 18:32:16 +0300

    playing with promises

--- helpers/promise-chaining.js
+++ helpers/promise-chaining.js
@@ -2,7 +2,7 @@ mongoose = require('../src/db/mongoose');
 const User = require('../src/models/user').User;
 const Task = require('../src/models/task').Task;
 
-
+/*
 User.findByIdAndUpdate('5caf99995aefe12c784ab3e3', { age: 1 }).then((user) => {
     console.log(user);
     return User.countDocuments({ age: 19 });
@@ -33,5 +33,24 @@ Task.findByIdAndDelete('5cc0552843fceda17d4d0867').then((task) => {
     //     console.log(task);
     // });
 }).catch((e)=>{console.log(e.message)});
+*/
 
+
+const updateAgeAndCount = async (id, age) => {
+
+    const user = User.findByIdAndUpdate(id, { age });
+
+    const count =  User.countDocuments( {age} );
+
+    return count;
+}
+
+
+updateAgeAndCount('5caf99995aefe12c784ab3e3', 120).then((count) => {
+
+    console.log(count);
+
+}).catch((e) => {
+    console.log(e.message);
+});
     

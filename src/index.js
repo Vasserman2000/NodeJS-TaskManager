8a911af90b882b70dac019c3b83a23c6fd6115f5
Vasserman2000 <vasserman2000@gmail.com>
2019-05-06 18:34:41 +0200

    separate file for tasks calls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('../src/db/mongoose');
-const userRouter = require('../src/routers/user')
+const userRouter = require('../src/routers/user');
+const taskRouter = require('../src/routers/task');
 
 
 const app = express();
@@ -10,82 +11,8 @@ const port = process.env.PORT || 3000;
 // parse http request body to json?
 app.use(express.json());
 app.use(userRouter.router);
+app.use(taskRouter.router);
 
-
-app.post('/tasks', (req, res) => {
-    const task = Task.Task(req.body);
-
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch((error) => {
-        res.status(400).send(error);
-    });
-});
-
-
-app.get('/tasks', (req, res) => {
-    Task.find().then((tasks) => {
-        res.send(tasks);
-    }).catch((error) => {
-        res.status(500).send(error.message);
-    });
-});
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id;
-
-    Task.findById(_id).then((task) => {
-        if (!task) {
-            return res.status(404).send();
-        }
-
-        res.send(task);
-    }).catch((error) => {
-        res.status(500).send();
-    });
-});
-
-
-app.patch('/tasks/:id', async (req, res) => {
-    const allowedUpdates = ['description', 'completed'];
-    const updates = Object.keys(req.body);
-
-    const isValidOperation = updates.every((prop) => allowedUpdates.includes(prop)) && updates.length > 0;
-
-    console.log(isValidOperation + ', updates: ' + updates.length)
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'invalid input'});
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-
-        if (!task) {
-            return res.status(404).send();
-        }
-
-        res.send(task);
-
-    } catch (e) {
-        res.status(400).send(e.message);
-    }
-
-});
-
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id);
-        if (!task) {
-            return res.status(404).send('A task with provided ID has not found');
-        }
-
-        res.send(task);
-    } catch(e) {
-        res.status(500).send();
-    }
-});
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,1 +1,78 @@
-const Task = require('../src/models/task').Task;
+const express = require('express');
+const Task = require('../models/task').Task;
+const router = new express.Router();
+
+router.post('/tasks', (req, res) => {
+    const task = Task.Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+
+router.get('/tasks', (req, res) => {
+    Task.find().then((tasks) => {
+        res.send(tasks);
+    }).catch((error) => {
+        res.status(500).send(error.message);
+    });
+});
+
+router.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
+
+router.patch('/tasks/:id', async (req, res) => {
+    const allowedUpdates = ['description', 'completed'];
+    const updates = Object.keys(req.body);
+
+    const isValidOperation = updates.every((prop) => allowedUpdates.includes(prop)) && updates.length > 0;
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'invalid input'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+
+});
+
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send('A task with provided ID has not found');
+        }
+
+        res.send(task);
+    } catch(e) {
+        res.status(500).send();
+    }
+});
+
+module.exports = { router };

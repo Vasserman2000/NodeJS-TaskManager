be770e633337592c65c49fdaf5c689e55680f2e8
Vasserman2000 <vasserman2000@gmail.com>
2019-05-15 19:20:46 +0300

    jwt continue

--- src/index.js
+++ src/index.js
@@ -21,8 +21,12 @@ app.listen(port, () => {
 const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse');
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {expiresIn: '10 days'});
     console.log(token);
+
+    const data = jwt.verify(token, 'thisismynewcourse');
+
+    console.log(data);
 }
 
 myFunction();

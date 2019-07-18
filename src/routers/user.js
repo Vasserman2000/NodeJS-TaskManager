7dc6ddbbd0a25b6ffad99fe8463195aee1392059
Vasserman2000 <vasserman2000@gmail.com>
2019-07-18 23:45:23 +0300

    get avatar in browser

--- src/routers/user.js
+++ src/routers/user.js
@@ -141,4 +141,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     }
 });
 
+// Serve avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error('error');
+        }
+
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+});
+
 module.exports = { router };

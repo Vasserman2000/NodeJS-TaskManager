c7244686ee025e45b0e8d146b48e2dc74bd504d8
Vasserman2000 <vasserman2000@gmail.com>
2019-07-23 23:43:15 +0300

    sending emails 2

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,15 @@ const sendgridAPIKey = '';
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-    to: 'vasserman2000@gmail.com',
-    from: 'vasserman2000@gmail.com',
-    subject: 'This is my first creation!',
-    text: 'I hope this one actually gets to you.'
-});
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'elisha@mail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    });
+}
+
+module.exports = {
+    sendWelcomeEmail
+}

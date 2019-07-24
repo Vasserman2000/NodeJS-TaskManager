7fbd1f6dfde12672e0012e4cd8125b5ea1056af5
Vasserman2000 <vasserman2000@gmail.com>
2019-07-24 19:39:02 +0300

    sending emails 3

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendgridAPIKey = '';
+const sendgridAPIKey = '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';
 
 sgMail.setApiKey(sendgridAPIKey);
 
@@ -11,8 +11,20 @@ const sendWelcomeEmail = (email, name) => {
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     });
+    console.log('email sent')
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'elisha@mail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    });
+    console.log('cancelation email sent')
 }
 
 module.exports = {
-    sendWelcomeEmail
+    sendWelcomeEmail,
+    sendCancelationEmail
 }

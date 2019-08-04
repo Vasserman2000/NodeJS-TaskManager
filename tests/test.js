bb6e736c93a6e49aff5944ad8fdfe82776df2efa
Vasserman2000 <vasserman2000@gmail.com>
2019-08-04 21:49:43 +0300

    testing 3

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit } = require('../src/math.js');
+const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit, add } = require('../src/math.js');
 
 test('should calculate total with tip', () => {
     const total = calculateTip(10, .3);
@@ -15,9 +15,21 @@ test('should convert 0 C to 32 F', () => {
     expect(result).toBe(32);
 });
 
-test('async test demo', (done) => {
-    setTimeout(() => {
-        expect(1).toBe(2);
+test('Should add two numbers', (done) => {
+    add (2, 3).then((sum)=> {
+        expect(sum).toBe(5);
         done();
-    },2000);
+    })
 })
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(12, 38);
+    expect(sum).toBe(51);
+})
+
+// test('async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2);
+//         done();
+//     }, 2000);
+// })

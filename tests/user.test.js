b105634542de8e1d7106363d477dcde2ae89e4ab
Vasserman2000 <vasserman2000@gmail.com>
2019-08-08 18:00:06 +0300

    continuing testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,19 @@
 const request = require('supertest');
 const app = require('../src/app.js');
+const User = require('../src/models/user').User;
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!',
+    age: 20
+}
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+});
+
 
 test('Should sign up a user', async () => {
     await request(app).post('/users').send({

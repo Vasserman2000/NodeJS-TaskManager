9e2a1229da1da6052042244a8319a1f4477460e0
Vasserman2000 <vasserman2000@gmail.com>
2019-08-13 12:05:31 +0300

    testing 9

--- tests/user.test.js
+++ tests/user.test.js
@@ -103,3 +103,40 @@ test('Should not delete account for unauthenticated user', async () => {
         .send()
         .expect(401);
 });
+
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('myAvatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+
+test('Should update valid user fields', async () => {
+    const newName = 'Roboto';
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: newName
+        })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toEqual(newName);
+});
+
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'New York'
+        })
+        .expect(400);
+});
